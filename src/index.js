d70bcc1e01492be946f456d9024c5a91d7d4a925
Chris Nickel <chris.nickel.dev@gmail.com>
2020-06-10 11:43:47 -0400

    Created input form with state

--- src/index.js
+++ src/index.js
@@ -4,11 +4,30 @@ import ReactDOM from "react-dom";
 class App extends React.Component {
   constructor() {
     super();
+
+    this.state = {
+      todo: "",
+    };
   }
+
+  handleChange = (e) => {
+    this.setState({
+      todo: e.target.value,
+    });
+  };
+
   render() {
     return (
       <div className='app'>
         <h1>ToDo List</h1>
+        <form>
+          <input
+            type='text'
+            placeholder='Add Todo'
+            onChange={this.handleChange}
+            value={this.state.todo}
+          />
+        </form>
       </div>
     );
   }
