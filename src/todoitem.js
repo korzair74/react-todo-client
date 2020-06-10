fb0457f1531362c9c6be7d88c86566bd4c04fd52
Chris Nickel <chris.nickel.dev@gmail.com>
2020-06-10 14:00:49 -0400

    completed checkbox done onclick

--- src/todoitem.js
+++ src/todoitem.js
@@ -3,13 +3,41 @@ import React from "react";
 export default class TodoItem extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      done: props.todoData.done,
+    };
   }
+
+  toggleDone = () => {
+    fetch(`http://localhost:5000/todo/${this.props.todoData.id}`, {
+      method: "PATCH",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({
+        done: !this.state.done,
+      }),
+    })
+      .then(() => {
+        this.setState({
+          done: !this.state.done,
+        });
+      })
+      .catch((err) => {
+        console.log("Toggle Done Error", err);
+      });
+  };
 
   render() {
     return (
       <div className='todo-item'>
-        <input type='checkbox' />
-        <p>{this.props.todoData.title}</p>
+        <input
+          type='checkbox'
+          defaultChecked={this.state.done}
+          onClick={this.toggleDone}
+        />
+        <p className={this.state.done ? "done" : null}>
+          {this.props.todoData.title}
+        </p>
       </div>
     );
   }
